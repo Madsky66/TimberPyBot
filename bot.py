b023941b04949b960f21b4ff17867f533e10959f
Pierrick Mériel-Bussy <120613686+Madsky66@users.noreply.github.com>
2023-07-09 23:35:49 +0200

    v0.5.7

--- bot.py
+++ bot.py
@@ -1,56 +1,24 @@
 import logging
 import threading
 import time
-import traceback
 
 import numpy as np
 import pyautogui
 import webcolors
 import winsound
 from PIL import ImageGrab
-from pynput.keyboard import Controller, Key
 from pynput.mouse import Controller as MouseController
 
 LOGGER_FORMAT = "%(asctime)s %(levelname)s:%(message)s"
-LOGGER_FILE = "bot.log"
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-console_format = logging.Formatter(LOGGER_FORMAT)
-console_handler.setFormatter(console_format)
-logger.addHandler(console_handler)
-
-file_handler = logging.FileHandler(LOGGER_FILE)
-file_handler.setLevel(logging.INFO)
-file_format = logging.Formatter(LOGGER_FORMAT)
-file_handler.setFormatter(file_format)
-logger.addHandler(file_handler)
-
-logging.basicConfig(filename=LOGGER_FILE, level=logging.INFO, format=LOGGER_FORMAT)
-
-H_COVERAGE = 0.1
-V_COVERAGE = 1
+logging.basicConfig(level=logging.INFO, format=LOGGER_FORMAT)
 
 BASE_WAIT_TIME = 0.1
-FLASH_WAIT_TIME = 0.1
-NONE_WAIT_TIME = 0.01
-BOTH_WAIT_TIME = 0.05
+FLASH_WAIT_TIME = 0.5
 
-COLOR_TO_DETECT = np.array([95, 41, 0])
 WHITE_COLOR = np.array([255, 255, 255])
-
-global hexcolor_output
-
-
-class ColorDetectionError(Exception):
-    pass
-
-
-def beep(frequency, duration):
-    winsound.Beep(frequency, duration)
+COLOR_TO_DETECT = np.array([95, 41, 0])
+H_COVERAGE = 0.1
+V_COVERAGE = 1
 
 
 def is_color_match(color, target_color, tolerance=0):
@@ -62,11 +30,10 @@ class Bot:
 
     def __init__(self, left_zones, right_zones):
         self.thread = threading.Thread(target=self.start)
-        self.keyboard = Controller()
         self.stop_detection_flag = False
         self.left_zones = left_zones
         self.right_zones = right_zones
-        self.confirmations = {"flash": 0, "aucun": 0, "double": 0, "gauche": 0, "droite": 0}
+        self.confirmations = {"droite": 0, "gauche": 0}
         self.mouse = MouseController()
 
     def visualize_zones(self):
@@ -78,118 +45,51 @@ class Bot:
             time.sleep(1)
 
     def detect_zone(self, zone_params, target_color, screenshot, tolerance=0):
-        try:
-            zone = screenshot.crop(zone_params.rect)
-            zone_np = np.array(zone)
-            total_pixels = zone.width * zone.height
-            matched_pixels = np.sum(is_color_match(zone_np, target_color, tolerance))
-            if np.array_equal(target_color, WHITE_COLOR):
-                return matched_pixels / total_pixels > 0.75
-            else:
-                coverage = matched_pixels / total_pixels
-                detected_color = zone.getpixel((0, 0))
-                color_hex = webcolors.rgb_to_hex(detected_color)
-                Bot.hexcolor_output = color_hex
-                return coverage >= zone_params.coverage
-        except Exception as e:
-            logging.error(f"Erreur lors de la détection de la zone : {str(e)}")
-            logging.error(traceback.format_exc())
-            raise
+        zone = screenshot.crop(zone_params.rect)
+        zone.save("zone.png")
+        # zone.show()
+        zone_np = np.array(zone)
+        total_pixels = zone.width * zone.height
+        matched_pixels = np.sum(is_color_match(zone_np, target_color, tolerance))
+        coverage = matched_pixels / total_pixels
+        detected_color = zone.getpixel((0, 0))
+        color_hex = webcolors.rgb_to_hex(detected_color)
+        Bot.hexcolor_output = color_hex
+        return coverage >= zone_params.coverage
 
     def check_game_state(self, left_zones, right_zones, screenshot):
-        try:
-            left_detected_colors = [zone for zone in left_zones if self.detect_zone(zone, COLOR_TO_DETECT, screenshot)]
-            right_detected_colors = [zone for zone in right_zones if self.detect_zone(zone, COLOR_TO_DETECT, screenshot)]
-            left_active = len(left_detected_colors) > 0
-            right_active = len(right_detected_colors) > 0
-            if not left_active and not right_active:
-                if all(self.detect_zone(zone, WHITE_COLOR, screenshot, tolerance=128) for zone in (left_zones + right_zones)):
-                    return "flash", None
-                else:
-                    return "aucun", "Jeu non actif"
-            elif left_active and right_active:
-                return "double", (left_detected_colors[0].color, right_detected_colors[0].color)
-            elif left_active:
-                return "droite", left_detected_colors[0].color
-            elif right_active:
-                return "gauche", right_detected_colors[0].color
-        except Exception as e:
-            logging.error(f"Erreur lors de la vérification de l'état du jeu : {str(e)}")
-            logging.error(traceback.format_exc())
-            raise
+        left_detected_colors = [zone for zone in left_zones if self.detect_zone(zone, COLOR_TO_DETECT, screenshot)]
+        right_detected_colors = [zone for zone in right_zones if self.detect_zone(zone, COLOR_TO_DETECT, screenshot)]
+        left_active = len(left_detected_colors) > 0
+        right_active = len(right_detected_colors) > 0
+
+        if left_active and not right_active:
+            return "droite", left_detected_colors[0].color
+        elif right_active and not left_active:
+            return "gauche", right_detected_colors[0].color
+        return None, None
 
     def handle_game_state(self, state, color):
-        try:
-            CONFIRMATIONS_REQUIRED = {
-                "droite": 1,
-                "gauche": 1,
-                "flash": 3,
-                "aucun": 1,
-                "double": 3,
-            }
-
-            beep_params = {
-                "droite": (1000, 500, 1500, 250),
-                "gauche": (1000, 500, 500, 250),
-                "flash": (1500, 500, 1500, 250),
-                "aucun": (250, 500),
-                "double": (100, 500),
-            }
-
-            wait_times = {
-                "droite": BASE_WAIT_TIME,
-                "gauche": BASE_WAIT_TIME,
-                "flash": FLASH_WAIT_TIME,
-                "aucun": NONE_WAIT_TIME,
-                "double": BOTH_WAIT_TIME,
-            }
-
-            keys_to_press = {
-                "droite": Key.left,
-                "gauche": Key.right,
-            }
-
-            self.confirmations[state] += 1
-            if self.confirmations[state] >= CONFIRMATIONS_REQUIRED[state]:
-                logging.warning(f" - {state.capitalize()} - Couleur : {color}")
-                for i in range(0, len(beep_params[state]), 2):
-                    beep(beep_params[state][i], beep_params[state][i + 1])
-                    time.sleep(0.01)
-                self.confirmations = {key: 0 for key in self.confirmations}
-                time.sleep(wait_times[state])
-            if state in keys_to_press and self.confirmations[state] >= CONFIRMATIONS_REQUIRED[state]:
-                self.press_key(keys_to_press[state])
-        except Exception as e:
-            logging.error(f"Erreur lors de la gestion de l'état du jeu : {str(e)}")
-            logging.error(traceback.format_exc())
-            raise
+        beep_params = {
+            "droite": (1000, 350, 1500, 350),
+            "gauche": (1000, 350, 500, 350),
+        }
+        wait_times = {
+            "droite": BASE_WAIT_TIME,
+            "gauche": BASE_WAIT_TIME,
+        }
+        for i in range(0, len(beep_params[state]), 2):
+            winsound.Beep(beep_params[state][i], beep_params[state][i + 1])
+            time.sleep(0.01)
+        logging.warning(f" - {state.capitalize()} - Couleur : {color}")
+        time.sleep(wait_times[state])
 
     def start(self):
         while not self.stop_detection_flag:
-            try:
-                screenshot = ImageGrab.grab()
-                game_state, color = self.check_game_state(self.left_zones, self.right_zones, screenshot)
+            screenshot = ImageGrab.grab()
+            game_state, color = self.check_game_state(self.left_zones, self.right_zones, screenshot)
+            if game_state is not None:
                 self.handle_game_state(game_state, color)
-            except ColorDetectionError as e:
-                logging.error(f"Une exception s'est produite lors de la détection de la couleur: {str(e)}")
-                self.stop()
-            except Exception as e:
-                logging.error(f"Erreur lors de l'exécution du bot : {str(e)}")
-                logging.error(traceback.format_exc())
-                self.stop()
-
-    def press_key(self, direction):
-        try:
-            if direction == Key.left:
-                pyautogui.press('left')
-                logging.info("Touche gauche pressée")
-            elif direction == Key.right:
-                pyautogui.press('right')
-                logging.info("Touche droite pressée")
-        except Exception as e:
-            logging.error(f"Erreur lors de la pression de la touche {direction} : {str(e)}")
-            logging.error(traceback.format_exc())
-            raise
 
     def stop(self):
         self.stop_detection_flag = True
@@ -197,7 +97,46 @@ class Bot:
             self.thread.join()
 
     def run(self):
-        logging.info("Démarrage du script")
+        logging.info(" Démarrage du script")
         time.sleep(1)
         self.visualize_zones()
         self.thread.start()
+
+    def press_key(self, key_to_press):
+        if key_to_press == "gauche":
+            pyautogui.press('left')
+            logging.info("Touche gauche pressée")
+        elif key_to_press == "droite":
+            pyautogui.press('right')
+            logging.info("Touche droite pressée")
+
+
+class ZoneParams:
+    def __init__(self, x, y, width, height, color, coverage):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.coverage = coverage
+        self.rect = (x, y, x + width, y + height)
+        self.confirmations = 0
+
+
+def main():
+    left_zones = [
+        ZoneParams(int(755 * 1.25), int(460 * 1.25), 12, 1, COLOR_TO_DETECT, H_COVERAGE),
+        ZoneParams(int(759 * 1.25), int(480 * 1.25), 1, 20, COLOR_TO_DETECT, V_COVERAGE),
+        ZoneParams(int(755 * 1.25), int(515 * 1.25), 12, 1, COLOR_TO_DETECT, H_COVERAGE)
+    ]
+    right_zones = [
+        ZoneParams(int(839 * 1.25), int(460 * 1.25), 12, 1, COLOR_TO_DETECT, H_COVERAGE),
+        ZoneParams(int(843 * 1.25), int(480 * 1.25), 1, 20, COLOR_TO_DETECT, V_COVERAGE),
+        ZoneParams(int(839 * 1.25), int(515 * 1.25), 12, 1, COLOR_TO_DETECT, H_COVERAGE)
+    ]
+    bot = Bot(left_zones, right_zones)
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,32 +1,3 @@
 from bot import Bot, COLOR_TO_DETECT, H_COVERAGE, V_COVERAGE
 
 
-class ZoneParams:
-    def __init__(self, x, y, width, height, color, coverage):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.coverage = coverage
-        self.rect = (x, y, x + width, y + height)
-        self.confirmations = 0
-
-
-def main():
-    left_zones = [
-        ZoneParams(int(755 * 1.25), int(460 * 1.25), 12, 1, COLOR_TO_DETECT, H_COVERAGE),
-        ZoneParams(int(759 * 1.25), int(480 * 1.25), 1, 20, COLOR_TO_DETECT, V_COVERAGE),
-        ZoneParams(int(755 * 1.25), int(515 * 1.25), 12, 1, COLOR_TO_DETECT, H_COVERAGE)
-    ]
-    right_zones = [
-        ZoneParams(int(839 * 1.25), int(460 * 1.25), 12, 1, COLOR_TO_DETECT, 0.1),
-        ZoneParams(int(843 * 1.25), int(480 * 1.25), 1, 20, COLOR_TO_DETECT, 0.75),
-        ZoneParams(int(839 * 1.25), int(515 * 1.25), 12, 1, COLOR_TO_DETECT, 0.1)
-    ]
-    bot = Bot(left_zones, right_zones)
-    bot.run()
-
-
-if __name__ == "__main__":
-    main()
