b71bdef8b2c090a5471e5421bb27b2482d491916
Pierrick Mériel-Bussy <120613686+Madsky66@users.noreply.github.com>
2023-07-11 09:28:27 +0200

    v0.6.1

--- launcher.py
+++ launcher.py
@@ -15,14 +15,14 @@ class Launcher:
         self.keyboard = Controller()
 
         logging.info(" Configuration des zones...")
-        self.left_main_zone = Zone(int(759 * 1.25), int(480 * 1.25), 1, 20)
-        self.right_main_zone = Zone(int(843 * 1.25), int(480 * 1.25), 1, 20)
+        self.left_main_zone = [Zone(int(759 * 1.25), int(480 * 1.25), 1, 20)]
+        self.right_main_zone = [Zone(int(843 * 1.25), int(480 * 1.25), 1, 20)]
         self.main_zones = self.left_main_zone, self.right_main_zone
         self.left_shake_zones = [Zone(int(755 * 1.25), int(460 * 1.25), 12, 1), Zone(int(755 * 1.25), int(515 * 1.25), 12, 1)]
         self.right_shake_zones = [Zone(int(839 * 1.25), int(460 * 1.25), 12, 1), Zone(int(839 * 1.25), int(515 * 1.25), 12, 1)]
         self.shake_zones = self.left_shake_zones, self.right_shake_zones
         self.zones = self.main_zones, self.shake_zones
-        self.visualize_zones()
+        self.visualize_zones(self.main_zones)
 
         logging.info(" Initialisation du bot...")
         time.sleep(0.5)
@@ -30,7 +30,7 @@ class Launcher:
         logging.info(" Lancement de la détection...")
         time.sleep(0.5)
 
-    def visualize_zones(self):
+    def visualize_zones(self, zones):
         logging.info(" Début de la visualisation des zones dans 3 secondes...")
         time.sleep(1)
         logging.info(" Début de la visualisation des zones dans 2 secondes...")
@@ -38,8 +38,10 @@ class Launcher:
         logging.info(" Début de la visualisation des zones dans 1 seconde...")
         time.sleep(1)
 
-        for zone in self.main_zones:
-            self.mouse.position = (zone.x, zone.y)
-            time.sleep(1)
-            self.mouse.position = ((zone.x + zone.width), (zone.y + zone.height))
-            time.sleep(2)
+        for zone_list in zones:
+            for zone in zone_list:
+                x, y, width, height = zone.x, zone.y, zone.width, zone.height
+                self.mouse.position = x, y
+                time.sleep(1)
+                self.mouse.position = (x + width), (y + height)
+                time.sleep(2)
