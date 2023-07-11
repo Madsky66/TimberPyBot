f6635ef9335a79a477de7d817e2b2b1dca12f876
Pierrick Mériel-Bussy <120613686+Madsky66@users.noreply.github.com>
2023-07-11 09:27:46 +0200

    v0.6.0

--- detection.py
+++ detection.py
@@ -1,6 +1,5 @@
 import logging
 import time
-import numpy as np
 import pyautogui
 import winsound
 
@@ -11,55 +10,55 @@ from const import WHITE_COLOR, BROWN_COLOR, SUCCESS_CONFIRMATIONS, FAIL_CONFIRMA
 
 
 def run(zones, left_main_zone, right_main_zone):
+    confirmations_counter = {Case.LEFT: 0, Case.RIGHT: 0, Case.FLASH: 0, Case.SHAKE: 0, Case.ERROR: 0}
     while True:
         main_zones, shake_zones = zones
-        if check("flash", WHITE_COLOR, main_zones, 16):
-            handle_action(Case.FLASH)
+        if check("flash", WHITE_COLOR, main_zones, tolerance=16):
+            handle_action(Case.FLASH, confirmations_counter)
         else:
-            if check("left", BROWN_COLOR, [left_main_zone], 0):
-                handle_action(Case.LEFT)
-            elif check("right", BROWN_COLOR, [right_main_zone], 0):
-                handle_action(Case.RIGHT)
-            elif not check("shake", BROWN_COLOR, shake_zones, 0):
-                handle_action(Case.SHAKE)
+            if check("left", BROWN_COLOR, [left_main_zone], tolerance=0):
+                handle_action(Case.LEFT, confirmations_counter)
+            elif check("right", BROWN_COLOR, [right_main_zone], tolerance=0):
+                handle_action(Case.RIGHT, confirmations_counter)
+            elif not check("shake", BROWN_COLOR, shake_zones, tolerance=0):
+                handle_action(Case.SHAKE, confirmations_counter)
             else:
-                handle_action(Case.ERROR)
+                handle_action(Case.ERROR, confirmations_counter)
 
 
-def check(case, color_to_check, zones_to_check, tolerance):
-    with ImageGrab.grab() as screenshot:
-        for zone in zones_to_check:
-            zone = screenshot.crop(zone.rect)
-            zone.show()
-            zone.save("zone.png")
-            total_pixels = zone.width * zone.height
+def check(case, color_to_check, zones, tolerance):
+    for zone_list in zones:
+        for zone in zone_list:
+            zone_image = ImageGrab.grab(zone.rect)
+
+            total_pixels = zone_image.width * zone_image.height
             matched_pixels = sum(
-                is_color_match(zone.getpixel((x, y)), color_to_check, tolerance)
-                for y in range(zone.height)
-                for x in range(zone.width)
+                is_color_match(zone_image.getpixel((x, y)), color_to_check, tolerance)
+                for y in range(zone_image.height)
+                for x in range(zone_image.width)
             )
-            if case == "shake":
+
+            if case == Case.SHAKE:
                 if (matched_pixels / total_pixels) >= SHAKE_COVERAGE:
                     return True
             else:
                 if (matched_pixels / total_pixels) >= MAIN_COVERAGE:
                     return True
-        return False
+
+    return False
 
 
 def is_color_match(pixel_to_check, decomposed_colors, tolerance):
-    if len(pixel_to_check) != len(decomposed_colors):
+    if not isinstance(pixel_to_check, tuple) or len(pixel_to_check) != len(decomposed_colors):
         return False
 
     return all(
-        abs(pixel_to_check[i] - color[i]) <= tolerance
-        for i, color in enumerate(decomposed_colors)
+        abs(pixel_to_check[i] - decomposed_colors[i]) <= tolerance
+        for i in range(len(decomposed_colors))
     )
 
 
-
-def handle_action(case):
-    confirmations_counter = {Case.LEFT: 0, Case.RIGHT: 0, Case.FLASH: 0, Case.SHAKE: 0, Case.ERROR: 0}
+def handle_action(case, counter):
     confirmations = {
         Case.LEFT: ("left", "right", " Branche détectée à droite", " DROITE -> "),
         Case.RIGHT: ("right", "left", " Branche détectée à gauche", " GAUCHE -> "),
@@ -67,28 +66,33 @@ def handle_action(case):
         Case.SHAKE: ("shake", None, " Tremblement en cours...", " SHAKE -> "),
         Case.ERROR: ("error", None, " - ERREUR -", " ERREUR -> "),
     }
-    _type, _action, _confirmed_message, _incremented_message = confirmations[case]
-    if confirmations[_type] == (SUCCESS_CONFIRMATIONS if case in [Case.LEFT, Case.RIGHT] else FAIL_CONFIRMATIONS):
+    _action, _confirmed_message, _incremented_message = confirmations[case][1:]
+
+    if counter[case] == (SUCCESS_CONFIRMATIONS if case in [Case.LEFT, Case.RIGHT] else FAIL_CONFIRMATIONS):
         logging.info(_confirmed_message)
         if case in [Case.LEFT, Case.RIGHT]:
+            logging.info("Branche détectée")
             time.sleep(SUCCESS_WAIT)
             pyautogui.press(_action)
-            confirmations_counter[_type] = 0
+            counter[case] = 0
         elif case == Case.FLASH:
+            logging.info("Flash détecté")
             winsound.Beep(1500, 250)
             time.sleep(FLASH_WAIT)
-            confirmations_counter[_type] = 0
+            counter[case] = 0
         elif case == Case.SHAKE:
+            logging.info("Tremblement détecté")
             winsound.Beep(500, 250)
             time.sleep(SHAKE_WAIT)
-            confirmations_counter[_type] = 0
+            counter[case] = 0
         else:
+            logging.info("Jeu non détecté")
             winsound.Beep(250, 250)
             time.sleep(FAIL_WAIT)
-            confirmations_counter[_type] = 0
+            counter[case] = 0
     else:
-        confirmations_counter[_type] += 1
+        counter[case] += 1
         for other_case in confirmations:
             if other_case != case:
-                confirmations_counter[confirmations[other_case][0]] = 0
-        logging.info(_incremented_message + str(confirmations[_type]))
+                counter[other_case] = 0
+        logging.info(_incremented_message + str(confirmations[case]))
