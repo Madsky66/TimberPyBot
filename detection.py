a3f7c37cb7d0d0f2bbe29a38b69748fe50f27921
Pierrick Mériel-Bussy <120613686+Madsky66@users.noreply.github.com>
2023-07-12 04:10:21 +0200

    v0.7.0

--- detection.py
+++ detection.py
@@ -4,74 +4,66 @@ import pyautogui
 import winsound
 
 from PIL import ImageGrab
-from init.case import Case
 from init.const import WHITE, BROWN
 
 
 def run(zones, count):
     while True:
-
         check_zones, verif_zones = zones
-        left_check_zone, right_check_zone = check_zones
-        if check(BROWN, check_zones, 0, 1) >= 50:
-            print(" Condition 1 : check(BROWN, check_zones, 0, 1) >= 50 is True")
+        l_check_zone, r_check_zone = check_zones
+        l_verif_zone_a, l_verif_zone_b, r_verif_zone_a, r_verif_zone_b = verif_zones
+        l_check_zone_img = ImageGrab.grab(l_check_zone.rect)
+        r_check_zone_img = ImageGrab.grab(r_check_zone.rect)
+        l_verif_zone_a_img = ImageGrab.grab(l_verif_zone_a.rect)
+        l_verif_zone_b_img = ImageGrab.grab(l_verif_zone_b.rect)
+        r_verif_zone_a_img = ImageGrab.grab(r_verif_zone_a.rect)
+        r_verif_zone_b_img = ImageGrab.grab(r_verif_zone_b.rect)
+        if check(BROWN, l_check_zone, 0, 1, l_check_zone_img) != 100 and check(BROWN, r_check_zone, 0, 1, r_check_zone_img) != 100:
             time.sleep(0.001)
-            if check(BROWN, [left_check_zone], 0, 1) == 100:
-                logging.info(" Condition 2 : check(BROWN, [left_check_zone], 0, 1) == 100 is True")
-                time.sleep(0.001)
-                handle_action(Case.LEFT)
-            elif check(BROWN, [right_check_zone], 0, 1) == 100:
-                logging.info(" Condition 2 : check(BROWN, [right_check_zone], 0, 1) == 100 is True")
-                time.sleep(0.001)
-                handle_action(Case.RIGHT)
-        else:
-            logging.info(" Condition 1 : check(BROWN, check_zones, 0, 1) >= 50 is False")
-            if check(WHITE, check_zones, 16, 1) == 100:
-                logging.info(" Condition 2 : check(WHITE, check_zones, 16, 1) == 100 is True")
+            if check(WHITE, l_verif_zone_a, 16, 1, l_verif_zone_a_img) == 100 and check(WHITE, l_verif_zone_b, 16, 1, l_verif_zone_b_img) == 100 and check(WHITE, r_verif_zone_a, 16, 1, r_verif_zone_a_img) == 100 and check(WHITE, r_verif_zone_b, 16, 1, r_verif_zone_b_img) == 100:
                 time.sleep(0.001)
                 if count == 3:
-                    count += 1
-                    handle_action(Case.FLASH)
+                    handle_action("FLASH")
                     count -= 3
                 else:
                     count += 1
                     print(f" Flash = {count}")
-            elif check(BROWN, verif_zones, 0, 1) >= 10:
-                logging.info(" Condition 2 : check(BROWN, verif_zones, 0, 1) >= 10 is True")
+            elif check(BROWN, l_verif_zone_a, 0, 1, l_verif_zone_a_img) >= 10 or check(BROWN, l_verif_zone_b, 0, 1, l_verif_zone_b_img) >= 10 and check(BROWN, r_verif_zone_a, 0, 1, r_verif_zone_a_img) >= 10 or check(BROWN, r_verif_zone_b, 0, 1, r_verif_zone_b_img) >= 10:
                 time.sleep(0.001)
                 if count == 3:
-                    count += 1
-                    handle_action(Case.SHAKING)
+                    handle_action("SHAKING")
                     count -= 3
                 else:
                     count += 1
                     print(f" Tremblements = {count}")
-            elif check(BROWN, verif_zones, 0, 1) == 0:
-                logging.info(" Condition 2 : check(BROWN, verif_zones, 0, 1) == 0 is True")
+            elif check(BROWN, l_verif_zone_a, 0, 1, l_verif_zone_a_img) == 0 or check(BROWN, l_verif_zone_b, 0, 1, l_verif_zone_b_img) == 0 and check(BROWN, r_verif_zone_a, 0, 1, r_verif_zone_a_img) == 0 or check(BROWN, r_verif_zone_b, 0, 1, r_verif_zone_b_img) == 0:
                 time.sleep(0.001)
-                handle_action(Case.INACTIVE)
+                handle_action("INACTIVE")
             else:
-                logging.info(" Condition 2 : check(BROWN, verif_zones, 0, 1) >= 10 is False")
-                handle_action(Case.ERROR)
+                handle_action("ERROR")
+        elif check(BROWN, l_check_zone, 0, 1, l_check_zone_img) == 100:
+            time.sleep(0.001)
+            handle_action("LEFT")
+        elif check(BROWN, r_check_zone, 0, 1, r_check_zone_img) == 100:
+            time.sleep(0.001)
+            handle_action("RIGHT")
+        else:
+            handle_action("ERROR")
 
 
-def check(color_to_check, zones, tolerance, coverage):
-    for zone in zones:
-        print(zone, zones)
-        print(f" Vérification en cours : {color_to_check}, Zone(s) : {str(zones)}, Tolerance : {tolerance}, Couverture : {coverage}")
-        zone_image = ImageGrab.grab(zone.rect)
-        total_pixels = zone_image.width * zone_image.height
-        matched_pixels = sum(
-            is_color_match(zone_image.getpixel((x, y)), color_to_check, tolerance)
-            for y in range(zone_image.height)
-            for x in range(zone_image.width)
-        )
-        result = ((matched_pixels / total_pixels) / coverage) * 100
-        print(f" Zone vérifiée. Correspondance : {matched_pixels} = {result}%")
-        # zone_image.show()
-        time.sleep(0.001)
-        return result
-    return False
+def check(color_to_check, zone, tolerance, coverage, img):
+    print(f" Vérification en cours : {color_to_check}, Zone : {str(zone)}, Tolerance : {tolerance}, Couverture : {coverage}")
+    total_pixels = img.width * img.height
+    matched_pixels = sum(
+        is_color_match(img.getpixel((x, y)), color_to_check, tolerance)
+        for y in range(img.height)
+        for x in range(img.width)
+    )
+    result = ((matched_pixels / total_pixels) / coverage) * 100
+    print(f" Zone vérifiée. Correspondance : {matched_pixels} = {result}%")
+    # zone_image.show()
+    time.sleep(0.001)
+    return result
 
 
 def is_color_match(pixel_to_check, decomposed_colors, tolerance):
@@ -84,14 +76,14 @@ def is_color_match(pixel_to_check, decomposed_colors, tolerance):
 
 
 def handle_action(_case):
-    if _case in [Case.LEFT, Case.RIGHT]:
+    if _case in ["LEFT", "RIGHT"]:
         time.sleep(0.75)
-        pyautogui.press("right" if _case == Case.LEFT else "left")
-        logging.info(" Branche détectée à droite" if _case == Case.LEFT else " Branche détectée à gauche")
-    elif _case in [Case.FLASH, Case.SHAKING]:
+        pyautogui.press("left" if _case == "LEFT" else "right")
+        logging.info(" Branche détectée à droite" if _case == "LEFT" else " Branche détectée à gauche")
+    elif _case in ["FLASH", "SHAKING"]:
         time.sleep(0.5)
-        winsound.Beep(1500 if _case == Case.FLASH else 500, 250)
-        logging.info(" Flash en cours..." if _case == Case.FLASH else " Tremblement en cours...")
+        winsound.Beep(1500 if _case == "FLASH" else 500, 250)
+        logging.info(" Flash en cours..." if _case == "FLASH" else " Tremblement en cours...")
     else:
-        logging.info(" Jeu inactif..." if _case == Case.INACTIVE else " - ERREUR -")
+        logging.info(" Jeu inactif..." if _case == "INACTIVE" else " - ERREUR -")
         time.sleep(1)
